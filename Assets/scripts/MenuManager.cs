b48ba528090a79abb5c3e23ddf111185e92386e1
sharani97 <73841513+sharani97@users.noreply.github.com>
2023-02-08 15:45:25 +0100

    Initial_push

---
+++ Assets/scripts/MenuManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+
+public class MenuManager : MonoBehaviourPunCallbacks {
+    [Header(" — -Menus — -")]
+    public GameObject mainMenu;
+    public GameObject lobbyMenu;
+    [Header(" — -Main Menu — -")]
+    public Button createRoomBtn;
+    public Button joinRoomBtn;
+    [Header(" — -Lobby Menu — -")]
+    public TMP_Text roomName;
+    public TMP_Text playerListText;
+    public string playerList="";
+    public Button startGameBtn;
+    public string GameName= "Game";
+
+    private void Start() {
+        createRoomBtn.interactable = false;
+        joinRoomBtn.interactable = false;
+        SetMenu(mainMenu);
+    }
+    public override void OnConnectedToMaster() {
+        createRoomBtn.interactable = true;
+        joinRoomBtn.interactable = true;
+    }
+    void SetMenu(GameObject menu) {
+        mainMenu.SetActive(false);
+        lobbyMenu.SetActive(false);
+        menu.SetActive(true);
+    }
+    public void OnCreateRoomBtn(TMP_Text roomNameInput) {
+      Debug.Log("Creating Room : " + roomNameInput.text); 
+      NetworkManager.instance.CreateRoom(roomNameInput.text);
+      roomName.SetText(roomNameInput.text);
+    }
+    public void OnJoinRoomBtn(TMP_Text roomNameInput) {
+        NetworkManager.instance.JoinRoom(roomNameInput.text);
+        Debug.Log("Join Room : " + roomNameInput.text);
+        roomName.SetText(roomNameInput.text);
+    }
+    public void OnPlayerNameUpdate(TMP_Text playerNameInput) {
+        PhotonNetwork.NickName = playerNameInput.text;
+        //Debug.Log("my playerN:" + PhotonNetwork.NickName);
+    }
+    public override void OnJoinedRoom() {
+        SetMenu(lobbyMenu);
+        Debug.Log("Room joined, updating LobbyUI");
+        photonView.RPC("UpdateLobbyUI", RpcTarget.All);
+    }
+    public override void OnPlayerLeftRoom(Player otherPlayer) {
+        UpdateLobbyUI();
+    }
+    
+
+    [PunRPC]
+    public void UpdateLobbyUI() {
+        Debug.Log("Updating lobby!");
+        playerList = "";
+        foreach (Player player in PhotonNetwork.PlayerList) {
+            if (player.IsMasterClient) {
+                Debug.Log("Master player is:" + player.NickName);
+                playerList += player.NickName + " (Host)\n";
+                //Debug.Log("Player list (master) : " + playerList);
+            }
+            else { playerList += player.NickName + "\n"; }
+        }
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.Log("Found Photon Master !");
+            startGameBtn.interactable = true;
+        }
+        else {
+            startGameBtn.interactable = false;
+        }
+        //Debug.Log("Player list : " + playerList);
+        playerListText.SetText(playerList);
+    }
+    public void OnLeaveLobbyBtn() {
+        PhotonNetwork.LeaveRoom();
+        SetMenu(mainMenu);
+    }
+    public void OnStartGameBtn() {
+        NetworkManager.instance.photonView.RPC("ChangeScene", RpcTarget.All, GameName);
+    }
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+
+public class PlayerController : MonoBehaviourPunCallbacks
+{
+    [HideInInspector]
+    public int id;
+    [Header("Component")]
+    public Rigidbody rig;
+    public Player photonPlayer;
+    public string playerNickName;
+    [SerializeField]
+    private float speed = 0.2f;
+    [PunRPC]
+    public void Initialize(Player player) {
+        photonPlayer = player;
+        id = player.ActorNumber;
+        speed = 0.2f;
+        GameManager.instance.players[id - 1] = this;
+        if (!photonView.IsMine) {
+            rig.isKinematic = true;
+        }
+    }
+    private void Start() {
+        speed = 0.2f;
+        rig.isKinematic = true;
+        Debug.Log("in game, player is: " + photonPlayer.NickName);
+        playerNickName = photonPlayer.NickName;
+    }
+
+    private void Update() { 
+        if (photonPlayer.IsLocal) { 
+            Movements(); 
+            if (Input.GetKey(KeyCode.LeftControl) || Input.GetAxisRaw("Shoot") !=0) { 
+                photonView.RPC("Fire", RpcTarget.All); 
+            } 
+        } 
+    }
+    void Movements() { 
+        float horizontal = Input.GetAxisRaw("Horizontal"); 
+        float vertical = Input.GetAxisRaw("Vertical"); 
+        float hori = Input.GetAxis("Horizontal"); 
+        float verti = Input.GetAxis("Vertical"); 
+        if (horizontal != 0 || vertical != 0 || hori != 0 || verti != 0) { 
+            speed = 0.2f; 
+        } 
+        else { 
+            speed = 0; 
+        } 
+        if ((horizontal > 0 && vertical > 0) || (hori > 0 && verti > 0)) { 
+            transform.localEulerAngles = new Vector3(0, 45, 0); 
+            transform.Translate(Vector3.forward * speed * Time.deltaTime); 
+        } 
+        else if ((horizontal > 0 && vertical < 0) || (hori > 0 && verti < 0)) { 
+            transform.localEulerAngles = new Vector3(0, 135, 0); 
+            transform.Translate(Vector3.forward * speed * Time.deltaTime); 
+        } 
+        else if ((horizontal < 0 && vertical < 0) || (hori < 0 && verti < 0)) { 
+            transform.localEulerAngles = new Vector3(0, -135, 0); 
+            transform.Translate(Vector3.forward * speed * Time.deltaTime); 
+        } 
+        else if ((horizontal < 0 && vertical > 0) || (hori < 0 && verti > 0)) { 
+            transform.localEulerAngles = new Vector3(0, -45, 0); 
+            transform.Translate(Vector3.forward * speed * Time.deltaTime); 
+        } 
+        else if ((horizontal > 0 && vertical == 0) || (hori > 0 && verti == 0)) {
+            transform.localEulerAngles = new Vector3(0, 90, 0); 
+            transform.Translate(Vector3.forward * speed * Time.deltaTime); 
+        } 
+        else if ((horizontal < 0 && vertical == 0) || (hori < 0 && verti == 0)) { 
+            transform.localEulerAngles = new Vector3(0, -90, 0); 
+            transform.Translate(Vector3.forward * speed * Time.deltaTime); 
+        } 
+        else if ((vertical > 0 && horizontal == 0) || (verti > 0 && hori == 0)) { 
+            transform.localEulerAngles = new Vector3(0, 0, 0); 
+            transform.Translate(Vector3.forward * speed * Time.deltaTime); 
+        } 
+        else if ((vertical < 0 && horizontal == 0) || (verti < 0 && hori == 0)) { 
+            transform.localEulerAngles = new Vector3(0, 180, 0); 
+            transform.Translate(Vector3.forward * speed * Time.deltaTime); 
+        } 
+    }
+
+    [PunRPC] 
+    void Fire() { 
+        GameObject bullet = Instantiate(Resources.Load("Bullet", typeof(GameObject))) as GameObject; 
+        bullet.name = photonPlayer.NickName; 
+        Rigidbody rb = bullet.GetComponent<Rigidbody>(); 
+        bullet.transform.localPosition = transform.position; 
+        bullet.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f); 
+        rb.AddForce(this.transform.forward * 300f); Destroy(bullet, 1); 
+    }
+    private void OnTriggerEnter(Collider other) { 
+        if (other.tag == "bullet")
+        { 
+            if (other.name != photonPlayer.NickName) { 
+                Debug.Log("hit !"); 
+                StartCoroutine(PlayerColorChange());
+            } 
+        } 
+    }
+    IEnumerator PlayerColorChange() { 
+        this.gameObject.GetComponent<MeshRenderer>().material.color = Color.red; 
+        yield return new WaitForSeconds(2); 
+        this.gameObject.GetComponent<MeshRenderer>().material.color = Color.green;
+    }
+}
+
